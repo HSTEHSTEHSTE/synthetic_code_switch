7515e92cced9987cf8baf624332c97c81156fb95
Xinyuan Li <xli257@b17.clsp.jhu.edu>
2022-07-07 11:29:08 -0400

    Add new synthetic training sentence generation script

--- generate_synthetic_training_sentences.py
+++ generate_synthetic_training_sentences.py
@@ -1,38 +1,55 @@
-from nltk.translate import AlignedSent
-from nltk.translate.ibm2 import IBMModel2
 import pickle
 import random
+import tqdm
 
 def replace_words(input_list, target_list, replacements):
-    for item in input_list:
-        if input_list.index(item) in replacements:
-            for replacement_item in replacements[input_list.index(item)]:
-                yield target_list[replacement_item]
+    for item_index, item in enumerate(input_list):
+        if str(item_index) in replacements:
+            for replacement_item in replacements[str(item_index)]:
+                yield target_list[int(replacement_item)]
         else:
             yield item
 
 
-bitext = pickle.load(open('/home/xli257/synthetic_code_switch/bitext', 'rb'))
-generated_data_file = open('/home/xli257/synthetic_code_switch/generated.txt', 'w')
+bitext = open('/home/xli257/synthetic_code_switch/data/parallel.txt', 'r')
+generated_data_file = open('/home/xli257/synthetic_code_switch/data/parallel_generated.txt', 'w')
+alignments = open('/home/xli257/synthetic_code_switch/data/parallel_align.txt', 'r')
 codeswitch_ratio = .5
 sigma = .4
-for bitext_element in bitext:
-    alignments = bitext_element.alignment
-    replacement_chance = random.gauss(codeswitch_ratio, sigma)
-    alignment_dictionary = {}
-    generated_sentence = bitext_element.mots
-    for alignment in alignments:
-        if alignment[1] in alignment_dictionary:
-            alignment_dictionary[alignment[1]].append(alignment[0])
+repeat = 8
+for bitext_element in tqdm.tqdm(bitext, total = 162026):
+    sentences = bitext_element.strip().split('|||')
+    sentence_zh = sentences[0].split()
+    sentence_en = sentences[1].split()
+
+    alignment_sentence = alignments.readline().strip().split()
+
+    for repeat_index in range(repeat):
+        replacement_chance = random.gauss(codeswitch_ratio, sigma)
+        alignment_dictionary = {}
+        if random.random() > .5:
+            generated_sentence = sentence_en
+            target_sentence = sentence_zh
+            original_language_id = 1
+            target_language_id = 0
         else:
-            alignment_dictionary[alignment[1]] = [alignment[0]]
-    replacement_dictionary = {}
-    for key in alignment_dictionary:
-        replacement_dice = random.random()
-        if replacement_dice > replacement_chance:
-            replacement_dictionary[key] = alignment_dictionary[key]
-            replacement_dictionary[key].sort()
-    codeswitched_sentence = list(replace_words(bitext_element.mots, bitext_element.words, replacement_dictionary))
-    generated_data_file.write('  '.join(codeswitched_sentence) + '\n')
+            generated_sentence = sentence_zh
+            target_sentence = sentence_en
+            original_language_id = 0
+            target_language_id = 1
+        for alignment in alignment_sentence:
+            alignment = alignment.split('-')
+            if alignment[original_language_id] in alignment_dictionary:
+                alignment_dictionary[alignment[original_language_id]].append(alignment[target_language_id])
+            else:
+                alignment_dictionary[alignment[original_language_id]] = [alignment[target_language_id]]
+        replacement_dictionary = {}
+        for key in alignment_dictionary:
+            replacement_dice = random.random()
+            if replacement_dice > replacement_chance:
+                replacement_dictionary[key] = alignment_dictionary[key]
+                replacement_dictionary[key].sort()
+        codeswitched_sentence = list(replace_words(generated_sentence, target_sentence, replacement_dictionary))
+        generated_data_file.write(''.join(codeswitched_sentence) + '\n')
 
 generated_data_file.close()
